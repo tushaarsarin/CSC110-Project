d4617837ba4f1cba061c8bb7843283be73bb59bb
Rohh1 <rohanssahota@gmail.com>
2021-12-14 01:06:09 -0500

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -9,21 +9,24 @@ def get_data(data: list[OneMonthData], categories_to_plot: list[str]) -> dict[st
         values[category] = (list(), list())
         for one_month_data in data:
             values[category][0].append(getattr(one_month_data, category))
-            values[category][1].append(one_month_data.date.month)
+            time = one_month_data.date
+            values[category][1].append(time.strftime("%m/%d/%Y"))
+
     return values
 
 
 def generate_graph(data: list[OneMonthData], categories_to_plot: list[str]) -> None:
     """Creates a scatter plot graph of the categories in categories_to_plot using the data in data."""
     filtered_data = get_data(data, categories_to_plot)
-    title = ', '.join(categories_to_plot)
+    title = 'Graph of '+', '.join(categories_to_plot)
     plt.style.use('seaborn')
     for category in filtered_data:
-        plt.scatter(filtered_data[category][0], filtered_data[category][1], s=300, c=((random(), random(), random()),), marker='X')
+        plt.scatter(filtered_data[category][1], filtered_data[category][0], s=300, c=((random(), random(), random()),), marker='X')
     plt.xlabel('Date')
     plt.ylabel('Value')
+    plt.title(title)
+    plt.legend(categories_to_plot)
     plt.show()
-
 
 
 
